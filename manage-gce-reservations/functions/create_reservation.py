34c451c233c868a5422c41d5686aac687f56a91b
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-02-03 17:29:20 +0200

    Initial

---
+++ manage-gce-reservations/functions/create_reservation.py
@@ -0,0 +1,43 @@
+# from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import json
+import sys
+
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+#  Authenticate using Service Account
+SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+SERVICE_ACCOUNT_FILE = 'security/uri-test-38aacab75c2a.json'
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# ===================================================================
+
+# Local testing
+gce_service = build('compute', 'v1', credentials=credentials)
+# -------------------------------------------------------------
+
+# Run on GCP
+# gce_service = build('compute', 'v1')
+# -------------------------------------------------------------
+
+
+def create_reservation(request):
+    # request_json = request.get_json()
+    # project = request_json['project']
+    project = 'uri-test'
+    # zone = request_json['zone']
+    zone = 'us-east1-c'
+    body = {"description":"Demo1","specificReservation":{"count":1,"instanceProperties":{"machineType":"n1-standard-2"}},"name":"gce"}
+
+    try:
+        reservation = gce_service.reservations().insert(
+            project=project, zone=zone,body=body).execute()
+    except Exception as e:    
+        print(e)
+    else:
+        return reservation
+
+
+input1 = {"project": "uri-test"}
+create_reservation(input1)
---
+++ manage-gce-reservations/functions/get_reservation_status.py
@@ -0,0 +1,42 @@
+# from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import json
+import sys
+
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+#  Authenticate using Service Account
+SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+SERVICE_ACCOUNT_FILE = 'security/uri-test-38aacab75c2a.json'
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# ===================================================================
+
+# Local testing
+gce_service = build('compute', 'v1', credentials=credentials)
+# -------------------------------------------------------------
+
+# Run on GCP
+# gce_service = build('compute', 'v1')
+# -------------------------------------------------------------
+
+
+def get_reservation_status(request):
+    # request_json = request.get_json()
+    # project = request_json['project']
+    project = 'uri-test'
+    # zone = request_json['zone']
+    zone = 'us-east1-c'
+    # reservation_name = request_json['name']
+    reservation_name = 'gce2'
+
+    reservation = gce_service.reservations().get(
+        project=project, zone=zone,reservation=reservation_name).execute()
+    
+
+    return reservation
+
+
+input1 = ''
+get_reservation_status(input1)
