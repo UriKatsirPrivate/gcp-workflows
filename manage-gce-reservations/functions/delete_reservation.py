e9855aa4f7442078329f6b19fb86fc933a3816c7
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-02-24 20:27:07 +0200

    Remove local debug info

--- manage-gce-reservations/functions/delete_reservation.py
+++ manage-gce-reservations/functions/delete_reservation.py
@@ -1,35 +1,19 @@
-# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import json
 import sys
 
-from google.oauth2 import service_account
 import googleapiclient.discovery
 
-#  Authenticate using Service Account
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-SERVICE_ACCOUNT_FILE = 'security/uri-test-38aacab75c2a.json'
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-# ===================================================================
-
-# Local testing
-gce_service = build('compute', 'v1', credentials=credentials)
-# -------------------------------------------------------------
-
 # Run on GCP
-# gce_service = build('compute', 'v1')
+gce_service = build('compute', 'v1')
 # -------------------------------------------------------------
 
 
 def delete_reservation(request):
-    # request_json = request.get_json()
-    # project = request_json['project']
-    project = 'uri-test'
-    # zone = request_json['zone']
-    zone = 'us-central1-a'
-    reservation_name = 'gce1'
-    # reservation_name = request_json['body']['name']
+    request_json = request.get_json()
+    project = request_json['project']
+    zone = request_json['zone']
+    reservation_name = request_json['body']['name']
 
     try:
         reservation = gce_service.reservations().delete(
@@ -38,7 +22,3 @@ def delete_reservation(request):
         print(e)
     else:
         return reservation
-
-
-input1 = {"project": "uri-test"}
-delete_reservation(input1)
