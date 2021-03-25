b48a02cb946b6eaf343235d607c192cb7a99b5ec
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-03-25 11:22:06 +0200

    Initial

---
+++ run-workflow-cloud-run/docker-commands.sh
@@ -0,0 +1,3 @@
+docker build -t gcr.io/uri-test/run-workflow-cloud-run:v1 .
+docker push gcr.io/uri-test/run-workflow-cloud-run:v1
+gcloud run deploy run-workflow-cloud-run --image gcr.io/uri-test/run-workflow-cloud-run:v1 --platform managed --region us-central1
---
+++ run-workflow-cloud-run/main.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+from googleapiclient.discovery import build
+import json
+import sys
+
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+app = Flask(__name__)
+
+workflow_service = build('workflowexecutions', 'v1')
+
+
+@app.route("/")
+def hello_world():
+    # name = os.environ.get("NAME", "World")
+    # return "CloudRun Serverless myths"
+    parent = "projects/uri-test/locations/us-central1/workflows/CreateMachineImagesInAllZones"
+    body = {"argument": '{"project": "uri-test","zone": "us-east1-b"}'}
+
+    workflow = workflow_service.projects().locations().workflows(
+    ).executions().create(parent=parent, body=body).execute()
+
+    return workflow
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
