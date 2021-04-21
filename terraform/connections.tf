192bce0b8c9b541f9ef424833d1828e8ee4f26e2
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-04-21 12:12:00 +0300

    Specify provide version using required_providers

--- terraform/connections.tf
+++ terraform/connections.tf
@@ -2,13 +2,19 @@ provider "google" {
   credentials = file("../../terraform/uri-test-key.json")
   project     = "uri-test"
   region      = "us-east1"
-  version     = "~> 3.59.0"
+  # version     = "~> 3.59.0"
 }
 
 terraform {
   backend "gcs" {
     bucket = "uri-test-tf-state"
     prefix = "uri-test-tf-state/workflows"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.59"
+    }
   }
 }
 
